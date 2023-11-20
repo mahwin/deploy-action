ea727486bd43ababcf670434900d7c049542fce4
youseock <mahwin7085@gmail.com>
2023-11-20 22:58:31 +0900

    test3

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -8,24 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">배포성공</p>
+      <div>배포 테스트</div>
     </>
   );
 }
